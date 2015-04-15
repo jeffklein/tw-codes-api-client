ec49affb78e84c4b8d42cf8f9d658140496e5bf1
Jeff Klein <jklein@guidewire.com>
2015-04-15 15:52:30 -0700

    IDEA code reformat

--- src/main/java/org/jeffklein/turfwars/codes/client/TempCodeApiResponse.java
+++ src/main/java/org/jeffklein/turfwars/codes/client/TempCodeApiResponse.java
@@ -11,25 +11,25 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 public class TempCodeApiResponse {
-    private Date timestamp;
-    private Date nextUpdate;
-    private List<TempCode> codes;
+  private Date timestamp;
+  private Date nextUpdate;
+  private List<TempCode> codes;
 
-    public TempCodeApiResponse(Date timestamp, Date nextUpdate, List<TempCode> codes) {
-        this.timestamp = timestamp;
-        this.nextUpdate = nextUpdate;
-        this.codes = codes;
-    }
+  public TempCodeApiResponse(Date timestamp, Date nextUpdate, List<TempCode> codes) {
+    this.timestamp = timestamp;
+    this.nextUpdate = nextUpdate;
+    this.codes = codes;
+  }
 
-    public Date getTimestamp() {
-        return timestamp;
-    }
+  public Date getTimestamp() {
+    return timestamp;
+  }
 
-    public Date getNextUpdate() {
-        return nextUpdate;
-    }
+  public Date getNextUpdate() {
+    return nextUpdate;
+  }
 
-    public List<TempCode> getCodes() {
-        return codes;
-    }
+  public List<TempCode> getCodes() {
+    return codes;
+  }
 }
--- src/test/java/org/jeffklein/turfwars/codes/client/TempCodeApiTest.java
+++ src/test/java/org/jeffklein/turfwars/codes/client/TempCodeApiTest.java
@@ -13,56 +13,56 @@ import java.util.List;
  */
 public class TempCodeApiTest {
 
-    private TempCodeApiClient client = new TempCodeApiClient();
+  private TempCodeApiClient client = new TempCodeApiClient();
 
-    @Test
-    public void testGetCodes() throws Throwable {
-        List<TempCode> codeList = client.getCodes();
-        Assert.assertNotNull(codeList);
-        Assert.assertTrue(codeList.size() >= 1);
-        TempCode firstCode = codeList.get(0);
-        Assert.assertNotNull(firstCode);
-        Assert.assertTrue(String.valueOf(firstCode.getExpires().getTime()).startsWith("14"));
-        Assert.assertTrue(firstCode.getCode().startsWith("-"));
-    }
+  @Test
+  public void testGetCodes() throws Throwable {
+    List<TempCode> codeList = client.getCodes();
+    Assert.assertNotNull(codeList);
+    Assert.assertTrue(codeList.size() >= 1);
+    TempCode firstCode = codeList.get(0);
+    Assert.assertNotNull(firstCode);
+    Assert.assertTrue(String.valueOf(firstCode.getExpires().getTime()).startsWith("14"));
+    Assert.assertTrue(firstCode.getCode().startsWith("-"));
+  }
 
-    @Test
-    public void testGetTempCodeApiResponse() throws Throwable {
-        TempCodeApiResponse response = client.getTempCodeApiResponse();
-        Assert.assertNotNull(response.getTimestamp());
-        Assert.assertTrue(String.valueOf(response.getTimestamp().getTime()).startsWith("14"));
-        Assert.assertNotNull(response.getNextUpdate());
-        Assert.assertTrue(response.getTimestamp().before(response.getNextUpdate()));
+  @Test
+  public void testGetTempCodeApiResponse() throws Throwable {
+    TempCodeApiResponse response = client.getTempCodeApiResponse();
+    Assert.assertNotNull(response.getTimestamp());
+    Assert.assertTrue(String.valueOf(response.getTimestamp().getTime()).startsWith("14"));
+    Assert.assertNotNull(response.getNextUpdate());
+    Assert.assertTrue(response.getTimestamp().before(response.getNextUpdate()));
 
-        List<TempCode> codeList = response.getCodes();
-        Assert.assertNotNull(codeList);
-        Assert.assertTrue(codeList.size() >= 1);
-        TempCode firstCode = codeList.get(0);
-        Assert.assertNotNull(firstCode);
-        Assert.assertTrue(String.valueOf(firstCode.getExpires().getTime()).startsWith("14"));
-        Assert.assertTrue(firstCode.getCode().startsWith("-"));
-    }
+    List<TempCode> codeList = response.getCodes();
+    Assert.assertNotNull(codeList);
+    Assert.assertTrue(codeList.size() >= 1);
+    TempCode firstCode = codeList.get(0);
+    Assert.assertNotNull(firstCode);
+    Assert.assertTrue(String.valueOf(firstCode.getExpires().getTime()).startsWith("14"));
+    Assert.assertTrue(firstCode.getCode().startsWith("-"));
+  }
 
-    @Test
-    public void testGetBodyJson() throws Throwable {
-        JSONObject bodyJson = client.getBodyJson();
-        Assert.assertNotNull(bodyJson.getLong("timestamp"));
-        Assert.assertTrue(String.valueOf(bodyJson.getLong("timestamp")).startsWith("14"));
-        Assert.assertNotNull(bodyJson.getLong("next_update"));
-        Assert.assertTrue(bodyJson.getLong("timestamp") < bodyJson.getLong("next_update"));
+  @Test
+  public void testGetBodyJson() throws Throwable {
+    JSONObject bodyJson = client.getBodyJson();
+    Assert.assertNotNull(bodyJson.getLong("timestamp"));
+    Assert.assertTrue(String.valueOf(bodyJson.getLong("timestamp")).startsWith("14"));
+    Assert.assertNotNull(bodyJson.getLong("next_update"));
+    Assert.assertTrue(bodyJson.getLong("timestamp") < bodyJson.getLong("next_update"));
 
-        JSONArray codesArray = bodyJson.getJSONArray("codes");
-        Assert.assertNotNull(codesArray);
-        Assert.assertTrue(codesArray.length() >= 1);
-        JSONObject firstCode = codesArray.getJSONObject(0);
-        Assert.assertNotNull(firstCode);
-        Assert.assertTrue(String.valueOf(firstCode.getLong("expires")).startsWith("14"));
-        Assert.assertTrue(firstCode.getString("code").startsWith("-"));
-    }
+    JSONArray codesArray = bodyJson.getJSONArray("codes");
+    Assert.assertNotNull(codesArray);
+    Assert.assertTrue(codesArray.length() >= 1);
+    JSONObject firstCode = codesArray.getJSONObject(0);
+    Assert.assertNotNull(firstCode);
+    Assert.assertTrue(String.valueOf(firstCode.getLong("expires")).startsWith("14"));
+    Assert.assertTrue(firstCode.getString("code").startsWith("-"));
+  }
 
-    @Test
-    public void testDigestHasCorrectValue() throws NoSuchAlgorithmException {
-        String expectedDigest = "b2a73767d7aec111e675955cb0ce63605b4ec48d";
-        Assert.assertEquals(client.digest(), expectedDigest);
-    }
+  @Test
+  public void testDigestHasCorrectValue() throws NoSuchAlgorithmException {
+    String expectedDigest = "b2a73767d7aec111e675955cb0ce63605b4ec48d";
+    Assert.assertEquals(client.digest(), expectedDigest);
+  }
 }
