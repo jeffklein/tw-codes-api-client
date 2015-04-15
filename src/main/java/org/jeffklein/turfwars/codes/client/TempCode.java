69f58e5284866385a69aa8876c8561be1b43089c
Jeff Klein <jklein@guidewire.com>
2015-04-15 15:59:53 -0700

    Moved api keys into a property file

--- src/main/java/org/jeffklein/turfwars/codes/client/TempCode.java
+++ src/main/java/org/jeffklein/turfwars/codes/client/TempCode.java
@@ -6,19 +6,19 @@ import java.util.Date;
  * TODO: redo this with Jackson annotations
  */
 public class TempCode {
-    private Date expires;
-    private String code;
+  private Date expires;
+  private String code;
 
-    public TempCode(Date expires, String code) {
-        this.expires = expires;
-        this.code = code;
-    }
+  public TempCode(Date expires, String code) {
+    this.expires = expires;
+    this.code = code;
+  }
 
-    public Date getExpires() {
-        return expires;
-    }
+  public Date getExpires() {
+    return expires;
+  }
 
-    public String getCode() {
-        return code;
-    }
+  public String getCode() {
+    return code;
+  }
 }
--- src/main/java/org/jeffklein/turfwars/codes/client/TempCodeApiClient.java
+++ src/main/java/org/jeffklein/turfwars/codes/client/TempCodeApiClient.java
@@ -12,79 +12,81 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.ResourceBundle;
 
 /**
  * TurfWars Temp Code API Client reimplemented in Java.
  * The original implementation was done in PHP by Nick.
  */
 public class TempCodeApiClient {
-    public static final String url_invite_codes = "https://turfwarsapp.com/api/invite_codes";
 
-    public static final String my_api_key = "c7a60f0dc66d197de7d2c1e6fcbde589";
+  private static ResourceBundle bundle = ResourceBundle.getBundle("tw-api");
 
-    public static final String my_api_secret = "cd6f0c3ac654c2da30143fff85e9d037";
+  public static final String URL_INVITE_CODES = bundle.getString("url.invite.codes");
 
-    public List<TempCode> getCodes() throws NoSuchAlgorithmException, UnirestException, TurfWarsApiException {
-        return getTempCodeApiResponse().getCodes();
+  public static final String MY_API_KEY = bundle.getString("my.api.key");
+
+  public static final String MY_API_SECRET = bundle.getString("my.api.secret");
+
+  public List<TempCode> getCodes() throws NoSuchAlgorithmException, UnirestException, TurfWarsApiException {
+    return getTempCodeApiResponse().getCodes();
+  }
+
+  public TempCodeApiResponse getTempCodeApiResponse() throws TurfWarsApiException {
+    JSONObject bodyJson = getBodyJson();
+    Date timestamp = new Date(bodyJson.getLong("timestamp"));
+    Date nextUpdate = new Date(bodyJson.getLong("next_update"));
+
+    List<TempCode> tempCodes = new ArrayList<TempCode>();
+    JSONArray jsonCodes = bodyJson.getJSONArray("codes");
+    for (int i = 0; i < jsonCodes.length(); i++) {
+      JSONObject jsonCode = jsonCodes.getJSONObject(i);
+      Date expires = new Date(jsonCode.getLong("expires"));
+      String code = jsonCode.getString("code");
+      TempCode tempCode = new TempCode(expires, code);
+      tempCodes.add(tempCode);
     }
+    TempCodeApiResponse responseWrapper = new TempCodeApiResponse(timestamp, nextUpdate, tempCodes);
+    return responseWrapper;
+  }
 
-    public TempCodeApiResponse getTempCodeApiResponse() throws TurfWarsApiException {
-        JSONObject bodyJson = getBodyJson();
-        Date timestamp = new Date(bodyJson.getLong("timestamp"));
-        Date nextUpdate = new Date(bodyJson.getLong("next_update"));
-
-        List<TempCode> tempCodes = new ArrayList<TempCode>();
-        JSONArray jsonCodes = bodyJson.getJSONArray("codes");
-        for (int i = 0 ; i < jsonCodes.length() ; i++) {
-            JSONObject jsonCode = jsonCodes.getJSONObject(i);
-            Date expires = new Date(jsonCode.getLong("expires"));
-            String code = jsonCode.getString("code");
-            TempCode tempCode = new TempCode(expires, code);
-            tempCodes.add(tempCode);
-        }
-        TempCodeApiResponse responseWrapper = new TempCodeApiResponse(timestamp, nextUpdate, tempCodes);
-        return responseWrapper;
+  protected JSONObject getBodyJson() throws TurfWarsApiException {
+    HttpResponse<JsonNode> response;
+    try {
+      response = Unirest.get(URL_INVITE_CODES)
+          .header("X-api-key", MY_API_KEY)
+          .header("X-api-digest", digest())
+          .asJson();
+    } catch (UnirestException e) {
+      throw new TurfWarsApiException("Problem while getting URL: " + URL_INVITE_CODES, e);
     }
-
-    protected JSONObject getBodyJson() throws TurfWarsApiException {
-        HttpResponse<JsonNode> response;
-        try {
-            response = Unirest.get(url_invite_codes)
-                    .header("X-api-key", my_api_key)
-                    .header("X-api-digest", digest())
-                    .asJson();
-        }
-        catch (UnirestException e) {
-            throw new TurfWarsApiException("Problem while getting URL: "+url_invite_codes, e);
-        }
-        JsonNode node = response.getBody();
-        JSONObject jsonObj = node.getObject();
-        JSONObject headerObj = jsonObj.getJSONObject("header");
-        int headerResponseCode = headerObj.getInt("code");
-        if (headerResponseCode != 1) {
-            throw new TurfWarsApiException("API Error #"+headerResponseCode+": "+headerObj.getString("msg")+" when contacting API");
-        }
-        return jsonObj.getJSONObject("body");
+    JsonNode node = response.getBody();
+    JSONObject jsonObj = node.getObject();
+    JSONObject headerObj = jsonObj.getJSONObject("header");
+    int headerResponseCode = headerObj.getInt("code");
+    if (headerResponseCode != 1) {
+      throw new TurfWarsApiException("API Error #" + headerResponseCode + ": " + headerObj.getString("msg") + " when contacting API");
     }
+    return jsonObj.getJSONObject("body");
+  }
 
-    protected String digest() {
-        String digest;
-        try {
-            digest = sha1(my_api_key+"(.)(.)"+my_api_secret);
-        }
-        catch (NoSuchAlgorithmException e) {
-            throw new TurfWarsApiException("Unable to generate SHA1 hash.", e);
-        }
-        return digest;
+  protected String digest() {
+    String digest;
+    try {
+      digest = sha1(MY_API_KEY + "(.)(.)" + MY_API_SECRET);
+    } catch (NoSuchAlgorithmException e) {
+      throw new TurfWarsApiException("Unable to generate SHA1 hash.", e);
     }
+    return digest;
+  }
 
-    private String sha1(String input) throws NoSuchAlgorithmException {
-        MessageDigest mDigest = MessageDigest.getInstance("SHA1");
-        byte[] result = mDigest.digest(input.getBytes());
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < result.length; i++) {
-            sb.append(java.lang.Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
-        }
-        return sb.toString();
+  private String sha1(String input) throws NoSuchAlgorithmException {
+    MessageDigest mDigest = MessageDigest.getInstance("SHA1");
+    byte[] result = mDigest.digest(input.getBytes());
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < result.length; i++) {
+      sb.append(java.lang.Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
     }
+    return sb.toString();
+  }
 }
